dc7887c4f748c19edb31518145c8afe8ed4004c1
Carpentier loic <lo.carpentier@outlook.fr>
2025-08-21 04:28:05 +0200

    refactor analytics counter

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -1,41 +1,40 @@
 package com.hemebiotech.analytics;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.FileWriter;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
 
-public class AnalyticsCounter {
-	private static int headacheCount = 0;
-	private static int rashCount = 0;
-	private static int pupilCount = 0;
+public class AnalyticsCounter{
 	
-	public static void main(String args[]) throws Exception {
-		// first get input
-		BufferedReader reader = new BufferedReader (new FileReader("symptoms.txt"));
-		String line = reader.readLine();
-
-		//count sysmptom
-		while (line != null) {
-			System.out.println("symptom from file: " + line);
-			if (line.equals("headache")) {
-				headacheCount++;
-				System.out.println("number of headaches: " + headacheCount);
-			}
-			else if (line.equals("rash")) {
-				rashCount++;
-			}
-			else if (line.contains("pupils")) {
-				pupilCount++;
-			}
-
-			line = reader.readLine();	// get another symptom
+	 private ISymptomReader reader;
+	 private ISymptomWriter writer;
+	
+	 public AnalyticsCounter(ISymptomReader reader, ISymptomWriter writer) {
+	        this.reader = reader;
+	        this.writer = writer;
+	    }
+	
+	 public List<String> getSymptoms() {
+		    return reader.GetSymptoms();
 		}
-		
-		// next generate output
-		FileWriter writer = new FileWriter ("result.out");
-		writer.write("headache: " + headacheCount + "\n");
-		writer.write("rash: " + rashCount + "\n");
-		writer.write("dialated pupils: " + pupilCount + "\n");
-		writer.close();
+
+	 public Map<String, Integer> countSymptoms(List<String> symptoms) {
+		    Map<String, Integer> counts = new HashMap<>();
+		    for (String symptom : symptoms) {
+		        counts.put(symptom, counts.getOrDefault(symptom, 0) + 1);
+		    }
+		    return counts;
+		}
+	 
+	 
+	 
+	public Map<String, Integer> sortSymptoms(Map<String, Integer> symptoms) {
+	    return new TreeMap<>(symptoms);
 	}
+	
+	public void writeSymptoms(Map<String, Integer> symptoms) {
+	    writer.writeSymptoms(symptoms);
+	}
+	
 }
